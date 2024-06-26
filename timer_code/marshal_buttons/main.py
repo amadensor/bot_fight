74d5cb4c0e0c8d52e8066720ffab9b202e4e9264
Grant Johnson <grant@amadensor.com>
2024-06-25 21:24:02 -0600

    Go old school loop.   The IRQ hates me.

--- timer_code/marshal_buttons/main.py
+++ timer_code/marshal_buttons/main.py
@@ -14,47 +14,29 @@ msg_bus=functions.Bus()
 
 ready_light.on()
 
-def start_button(pin):
-    global action
-    action="start"
-
-def stop_button(pin):
-    global action
-    action="stop"
-
-def reset_button(pin):
-    global action
-    action="reset"
-
-def config_button(pin):
-    global action
-    action="config"
-
-def countdown_button(pin):
-    global action
-    action="countdown"
-
 start_pin=machine.Pin(start_pin_num,machine.Pin.IN, machine.Pin.PULL_DOWN)
-start_pin.irq(trigger=machine.Pin.IRQ_RISING, handler=start_button)
-
 countdown_pin=machine.Pin(countdown_pin_num,machine.Pin.IN, machine.Pin.PULL_DOWN)
-countdown_pin.irq(trigger=machine.Pin.IRQ_RISING, handler=countdown_button)
-
 reset_pin=machine.Pin(reset_pin_num,machine.Pin.IN, machine.Pin.PULL_DOWN)
-reset_pin.irq(trigger=machine.Pin.IRQ_RISING, handler=reset_button)
-
 config_pin=machine.Pin(config_pin_num,machine.Pin.IN, machine.Pin.PULL_DOWN)
-config_pin.irq(trigger=machine.Pin.IRQ_RISING, handler=config_button)
-
 stop_pin=machine.Pin(stop_pin_num,machine.Pin.IN, machine.Pin.PULL_DOWN)
-stop_pin.irq(trigger=machine.Pin.IRQ_RISING, handler=stop_button)
-
 
 def hardware_loop():
     global action
+    if start_pin.value():
+        action="start"
+    if countdown_pin.value():
+        action="countdown"
+    if reset_pin.value():
+        action="reset"
+    if config_pin.value():
+        action="config"
+    if stop_pin.value():
+        action="stop"
     if action:
         message={"button":action}
         msg_bus.send_message(message)
-        time.sleep(.1)
+        time.sleep(.25)
         action=None
     
+while True:
+    hardware_loop()
