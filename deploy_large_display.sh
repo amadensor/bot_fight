53c191e846a02a1e99d0d90de37e655cdbb130f6
Grant Johnson <grant@amadensor.com>
2024-09-08 19:49:08 -0600

    Fix deploy script for large display Fix pin numbers on RJ 45 common

--- deploy_large_display.sh
+++ deploy_large_display.sh
@@ -1,5 +1,5 @@
 rshell cp timer_code/functions.py /pyboard
 rshell cp timer_code/large_display/main.py /pyboard
 rshell mkdir /pyboard/lib
-rshell cp timer_code/micropython-tm1637/tm1637.py /pyboard/lib
+rshell cp timer_code/large_display/display_driver.py /pyboard/lib
 
